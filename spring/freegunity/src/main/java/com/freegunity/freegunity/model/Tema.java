a89de695747501ca7c82ae043b853d5c4fb12c43
João P. Cruz <jpcgomes0@gmail.com>
2021-08-27 09:34:23 -0300

    Criação do Spring e Model

---
+++ spring/freegunity/src/main/java/com/freegunity/freegunity/model/Tema.java
@@ -0,0 +1,63 @@
+package com.freegunity.freegunity.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank
+	@Size(min = 5, max = 55)
+	private String titulo;
+	
+	@NotBlank
+	@Size(min = 5, max = 255)
+	private String img;
+	
+	@NotBlank
+	@Size(min = 5, max = 255)
+	private String texto;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	
+}
